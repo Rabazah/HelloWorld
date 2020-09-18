d707d35cecca7d31c44bf79af505930b8a86bf03
TimBlendl <TimBlendl@0-208-01>
2020-09-18 12:53:47 +0200

    Counter

--- HelloWorld/src/HelloWorld.java
+++ HelloWorld/src/HelloWorld.java
@@ -1,10 +1,24 @@
 
 public class HelloWorld {
 
+	static int counter = 0;
+	
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
 		
-		//
+		
+		
+		while(counter<100) {
+		if(counter%5==0) { System.out.println("Hello HSV!");}
+		else {System.out.println("Hello World!");}
+		System.out.println(counter);
+		
+		counter++;
+		
+		}
+		
+		
+		
+		// 
 
 	}
 
