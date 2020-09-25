87fb976999f00d4378bae6333a2460e27b311eb6
TimBlendl <TimBlendl@0-208-01>
2020-09-25 11:59:23 +0200

    Fizz

--- HelloWorld/src/HelloWorld.java
+++ HelloWorld/src/HelloWorld.java
@@ -1,25 +1,25 @@
 
 public class HelloWorld {
 
-	static int counter = 0;
+	static int counter = 1;
 	
 	public static void main(String[] args) {
 		
 		
 		
-		while(counter<100) {
-		if(counter%5==0) { System.out.println("Hello HSV!");}
-		else {System.out.println("Hello World!");}
-		System.out.println(counter);
-		
-		counter++;
-		
+		while(counter<100){
+			if(counter%3==0 && counter%5==0) {
+				System.out.println("FizzBuzz");}
+			   else {    
+			     if(counter%3==0) { System.out.println("Fizz");}
+			        else {
+			            if(counter%5==0) {System.out.println("Buzz");}
+			                else {System.out.println(counter);}
+				}
+		     }
+		 counter++;
 		}
-		
-		
-		
-		// 
-
+	  } 
 	}
 
-}
+
